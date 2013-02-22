b5e7f9542a4b89fcf99a8af7d3d6369431d64fee
Chen XU <chenxu@thoughtworks.com>
2013-02-22 19:00:18 +0800

    revert change

--- app-android/src/zhang/abel/memmo/android/MainActivity.java
+++ app-android/src/zhang/abel/memmo/android/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends Activity
     public void createAlbum(View view){
         new AlertDialog.Builder(this)
                 .setTitle("新建记忆相册")
+                .setIcon(android.R.drawable.ic_dialog_info)
                 .setView(new EditText(this))
                 .setPositiveButton("好了", null)
                 .setNegativeButton("取消", null)
