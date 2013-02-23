bb79c2ba85946d8f9639973b0dcfe79ec29a4c79
Enyu <459969@qq.com>
2013-02-22 23:20:17 -0800

    memmo #3(enyu): add the user permission for creating directory, and add SD card check.

--- app-android/src/zhang/abel/memmo/android/MainActivity.java
+++ app-android/src/zhang/abel/memmo/android/MainActivity.java
@@ -30,16 +30,26 @@ public class MainActivity extends Activity
                 .setPositiveButton("好了", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        String path = getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).toString();
-                        String folderName = String.valueOf(albumName.getText());
-                        File folderPath = new File(path + File.separator + folderName);
+                        if (hasSDCard()) {
+                            String path = getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).toString();
+                            String folderName = String.valueOf(albumName.getText());
+                            File folderPath = new File(path + File.separator + folderName);
 
-                        if (!folderPath.exists()) {
-                            folderPath.mkdir();
+                            if (!folderPath.exists()) {
+                                folderPath.mkdir();
+                            }
+                        }else{
+                            //write the internal storage directory.
                         }
+
                     }
                 })
                 .setNegativeButton("取消", null)
                 .show();
+    }
+
+    private static boolean hasSDCard() {
+        String status = Environment.getExternalStorageState();
+        return status.equals(Environment.MEDIA_MOUNTED);
     }
 }
